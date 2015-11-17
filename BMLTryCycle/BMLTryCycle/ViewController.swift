139fb41bd5922c3fae4c6a453b150aa6da9218f6
mustafaalhayali <mustafaalhayali@hotmail.com>
2015-11-17 13:11:30 -0500

    Initial Commit

---
+++ BMLTryCycle/BMLTryCycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMLTryCycle
+//
+//  Created by Mustafa Al-Hayali on 2015-11-17.
+//  Copyright © 2015 Mustafa Al-Hayali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
