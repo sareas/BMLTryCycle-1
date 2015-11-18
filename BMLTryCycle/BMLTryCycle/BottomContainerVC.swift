0d2fd577bf39a0afc8cadfadfd6cce1803a1ec97
spenceralderson <spenceralderson@gmail.com>
2015-11-18 15:45:18 -0500

    added 2 container views with labels

---
+++ BMLTryCycle/BMLTryCycle/BottomContainerVC.swift
@@ -0,0 +1,35 @@
+//
+//  BottomContainerVC.swift
+//  BMLTryCycle
+//
+//  Created by Spencer Alderson on 2015-11-18.
+//  Copyright © 2015 Mustafa Al-Hayali. All rights reserved.
+//
+
+import UIKit
+
+class BottomContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
